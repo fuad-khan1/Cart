505af4a7fa0c09caab3a87056b308f30d63efdec
Fuad Khan <fuadkhanantor1@gmail.com>
2023-09-06 04:35:59 +0600

    updateCart function implented

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 const cart = [];
 let totalPrice = 0;
+
+const addToCart = document.querySelectorAll(".add-to-cart");
+const cartItemsList = document.getElementById("cart-items");
+
+const updateCart = () => {
+  cartItemsList.innerHTML = "";
+
+  cart.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `${item.name}`;
+    cartItemsList.appendChild(listItem);
+  });
+};
+
+addToCart.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    const productName =
+      document.querySelectorAll(".card h2")[index].textContent;
+    const product = { name: productName };
+    cart.push(product);
+    updateCart();
+  });
+});
