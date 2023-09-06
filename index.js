225c3e5c5127f9819a97960ade779b5428d44ada
Fuad Khan <fuadkhanantor1@gmail.com>
2023-09-06 22:01:15 +0600

    updated

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
 const cart = [];
 let totalPrice = 0;
 
-const addToCart = document.querySelectorAll(".add-to-cart");
 const totalPriceElement = document.getElementById("total-price");
 const cartItemsList = document.getElementById("cart-items");
 const cartPrompt = document.querySelector(".cart-section");
 const productSection = document.getElementById("product-section");
 const cartButton = document.getElementById("cart-button");
 
+
+ //------Update cart function ---------//
 const updateCart = () => {
   cartItemsList.innerHTML = "";
   totalPrice = 0;
 
   cart.forEach((item, index) => {
     const listItem = document.createElement("li");
-    listItem.innerHTML = `${item.name} => Price: ${item.price} <button class="rounded-lg p-1 hover:bg-red-500 bg-red-300 text-white mb-2 delete-item" data-index="${index}">Remove</button>`;
+    listItem.innerHTML = `
+    <img src="${item.thumbnail}" alt="image">
+    <p>${item.name} $${item.price} <button class="rounded-lg p-1 hover:bg-red-500 bg-red-300 text-white mb-2 delete-item" data-index="${index}">Remove</button></p>
+
+     
+      `;
     cartItemsList.appendChild(listItem);
     totalPrice += item.price;
   });
@@ -31,22 +37,24 @@ const updateCart = () => {
   });
 };
 
-// data fetching
- fetch("https://dummyjson.com/products")
-   .then((res) =>  res.json())
+//------- data fetching ---------------//
+fetch("https://dummyjson.com/products")
+  .then((res) => res.json())
   .then((data) => {
     console.log("Data=>", data);
     productSection.innerHTML = "";
 
-    data.products.forEach((product, index) => {
+    const productsToDisplay = data.products.slice(0, 6);
+
+    productsToDisplay.forEach((product, index) => {
       const card = document.createElement("div");
       card.className = "card w-80 rounded shadow-lg";
       card.innerHTML = `
-      <img src="${product.images[0]}" alt="Product img" class="w-full h-48 object-cover rounded-t-lg">
+      <img id="Product-img" src="${product.thumbnail}" alt="Product img" class="w-full h-48 object-cover rounded-t-lg">
       <div class="p-4">
         <h2 class="text-2xl font-semibold">${product.title}</h2>
         <p>Rating: ${product.rating}</p>
-        <p class=" mt-2">$${product.price}</p>
+        <p class="mt-2">$${product.price}</p>
         <button class="add-to-cart mt-4 bg-blue-400 text-white hover:bg-blue-600 px-4 py-2 rounded-full" data-index="${index}">Add to Cart</button>
       </div>
       
@@ -54,17 +62,21 @@ const updateCart = () => {
       `;
 
       productSection.appendChild(card);
-      card.querySelector(".add-to-cart").addEventListener("click", () => {
-        const productName =
-          document.querySelectorAll(".card h2")[index].textContent;
-        const productPrice = parseFloat(
-          document.querySelectorAll(".card p")[index].textContent
-        );
+      const addToCartButton = card.querySelector(".add-to-cart");
+      addToCartButton.addEventListener("click", () => {
+        // const productImg = document.getElementById("Product-img");
+
+        const productName = product.title;
+        const productPrice = product.price;
         const productIndex = cart.findIndex(
           (item) => item.name === productName
         );
         if (productIndex === -1) {
-          const product = { name: productName, price: productPrice };
+          const product = {
+            name: productName,
+            price: productPrice,
+            // image: productImg,
+          };
           cart.push(product);
           updateCart();
         } else {
@@ -74,25 +86,7 @@ const updateCart = () => {
     });
   });
 
-
-// addToCart.forEach((button, index) => {
-//   button.addEventListener("click", () => {
-//     const productName =
-//       document.querySelectorAll(".card h2")[index].textContent;
-//     const productPrice = parseFloat(
-//       document.querySelectorAll(".card p")[index].textContent
-//     );
-//     const productIndex = cart.findIndex((item) => item.name === productName);
-//     if (productIndex === -1) {
-//       const product = { name: productName, price: productPrice };
-//       cart.push(product);
-//       updateCart();
-//     } else {
-//       alert("This item is already added");
-//     }
-//   });
-// });
-
+  // ---------  -------//
 cartButton.addEventListener("click", () => {
   cartPrompt.classList.toggle("hidden");
 });
