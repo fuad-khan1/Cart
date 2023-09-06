116d7b6d8b4ecd8ef292718e328fb123faae1528
Fuad Khan <fuadkhanantor1@gmail.com>
2023-09-06 19:36:05 +0600

    API fetching added

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const addToCart = document.querySelectorAll(".add-to-cart");
 const totalPriceElement = document.getElementById("total-price");
 const cartItemsList = document.getElementById("cart-items");
 const cartPrompt = document.querySelector(".cart-section");
+const productSection = document.getElementById("product-section");
 const cartButton = document.getElementById("cart-button");
 
 const updateCart = () => {
@@ -30,23 +31,67 @@ const updateCart = () => {
   });
 };
 
-addToCart.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    const productName =
-      document.querySelectorAll(".card h2")[index].textContent;
-    const productPrice = parseFloat(
-      document.querySelectorAll(".card p")[index].textContent
-    );
-    const productIndex = cart.findIndex((item) => item.name === productName);
-    if (productIndex === -1) {
-      const product = { name: productName, price: productPrice };
-      cart.push(product);
-      updateCart();
-    } else {
-      alert("This item is already added");
-    }
+// data fetching
+ fetch("https://dummyjson.com/products")
+   .then((res) =>  res.json())
+  .then((data) => {
+    console.log("Data=>", data);
+    productSection.innerHTML = "";
+
+    data.products.forEach((product, index) => {
+      const card = document.createElement("div");
+      card.className = "card w-80 rounded shadow-lg";
+      card.innerHTML = `
+      <img src="${product.images[0]}" alt="Product img" class="w-full h-48 object-cover rounded-t-lg">
+      <div class="p-4">
+        <h2 class="text-2xl font-semibold">${product.title}</h2>
+        <p>Rating: ${product.rating}</p>
+        <p class=" mt-2">$${product.price}</p>
+        <button class="add-to-cart mt-4 bg-blue-400 text-white hover:bg-blue-600 px-4 py-2 rounded-full" data-index="${index}">Add to Cart</button>
+      </div>
+      
+    
+      `;
+
+      productSection.appendChild(card);
+      card.querySelector(".add-to-cart").addEventListener("click", () => {
+        const productName =
+          document.querySelectorAll(".card h2")[index].textContent;
+        const productPrice = parseFloat(
+          document.querySelectorAll(".card p")[index].textContent
+        );
+        const productIndex = cart.findIndex(
+          (item) => item.name === productName
+        );
+        if (productIndex === -1) {
+          const product = { name: productName, price: productPrice };
+          cart.push(product);
+          updateCart();
+        } else {
+          alert("This item is already added");
+        }
+      });
+    });
   });
-});
+
+
+// addToCart.forEach((button, index) => {
+//   button.addEventListener("click", () => {
+//     const productName =
+//       document.querySelectorAll(".card h2")[index].textContent;
+//     const productPrice = parseFloat(
+//       document.querySelectorAll(".card p")[index].textContent
+//     );
+//     const productIndex = cart.findIndex((item) => item.name === productName);
+//     if (productIndex === -1) {
+//       const product = { name: productName, price: productPrice };
+//       cart.push(product);
+//       updateCart();
+//     } else {
+//       alert("This item is already added");
+//     }
+//   });
+// });
 
 cartButton.addEventListener("click", () => {
   cartPrompt.classList.toggle("hidden");
