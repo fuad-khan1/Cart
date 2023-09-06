e5f8ca87a5caeff57411209e485737ec6e60be88
Fuad Khan <fuadkhanantor1@gmail.com>
2023-09-07 05:00:06 +0600

    Ui updated

--- index.js
+++ index.js
@@ -5,27 +5,28 @@ const totalPriceElement = document.getElementById("total-price");
 const cartItemsList = document.getElementById("cart-items");
 const cartPrompt = document.querySelector(".cart-section");
 const productSection = document.getElementById("product-section");
-const cartButton = document.getElementById("cart-button");
+const cartButton = document.getElementById("cart-icon");
 
-
- //------Update cart function ---------//
+//------Update cart function ---------//
 const updateCart = () => {
   cartItemsList.innerHTML = "";
   totalPrice = 0;
-
   cart.forEach((item, index) => {
     const listItem = document.createElement("li");
     listItem.innerHTML = `
-    <img src="${item.thumbnail}" alt="image">
-    <p>${item.name} $${item.price} <button class="rounded-lg p-1 hover:bg-red-500 bg-red-300 text-white mb-2 delete-item" data-index="${index}">Remove</button></p>
-
-     
+   <div class="flex items-center ">
+   <div><img src="${item.image}" class="w-14 mr-4 rounded"  alt="image"></div>
+   <div>
+   <p>${item.name} <i class="fa-solid fa-trash-can ml-3 delete-item  text-red-400 hover:text-red-700 cursor-pointer" data-index=${index} ></i></p>
+   <p> $${item.price}</p> 
+   </div>
+   </div>   
       `;
     cartItemsList.appendChild(listItem);
     totalPrice += item.price;
   });
-  totalPriceElement.innerHTML = `Total: $${totalPrice}`;
-  cartButton.innerHTML = `Cart <sup>${cart.length}</sup>`;
+  totalPriceElement.innerHTML = `Total Price: $${totalPrice}`;
+  cartButton.innerHTML = `<sup> ${cart.length}</sup>`;
 
   const deleteButtons = document.querySelectorAll(".delete-item");
   deleteButtons.forEach((button) => {
@@ -48,14 +49,21 @@ fetch("https://dummyjson.com/products")
 
     productsToDisplay.forEach((product, index) => {
       const card = document.createElement("div");
-      card.className = "card w-80 rounded shadow-lg";
+      card.className = "card w-80 rounded shadow-lg overflow-hidden";
       card.innerHTML = `
-      <img id="Product-img" src="${product.thumbnail}" alt="Product img" class="w-full h-48 object-cover rounded-t-lg">
+     
+      <img id="Product-img" src="${product.thumbnail}" alt="Product img" class="w-full h-48 object-cover rounded-t-lg transition-transform transform hover:-translate-y-1 hover:scale-110 duration-300 group-hover:scale-110 ">
+     
       <div class="p-4">
         <h2 class="text-2xl font-semibold">${product.title}</h2>
-        <p>Rating: ${product.rating}</p>
-        <p class="mt-2">$${product.price}</p>
-        <button class="add-to-cart mt-4 bg-blue-400 text-white hover:bg-blue-600 px-4 py-2 rounded-full" data-index="${index}">Add to Cart</button>
+
+        <p><i class="fa-solid fa-star text-yellow-400"></i> <i class="fa-solid fa-star text-yellow-400"></i> <i class="fa-solid fa-star text-yellow-400"></i> <i class="fa-solid fa-star text-yellow-400"></i> <i class="fa-solid fa-star-half-stroke text-yellow-400"></i></p>
+
+        <p class="my-2">$${product.price}</p>
+        
+        <i  class="add-to-cart fa-solid fa-bag-shopping text-xl border rounded-full  text-orange-500 px-2 py-1 cursor-pointer hover:bg-orange-500 hover:text-white hover:transition ease-in-out duration-700 "></i>
+        <i  class="fa-regular fa-eye text-xl border rounded-full  text-orange-500 px-2 py-1 cursor-pointer hover:bg-orange-500 hover:text-white hover:transition ease-in-out duration-700 "></i>
+        <i  class="fa-regular fa-heart text-xl border rounded-full  text-orange-500 px-2 py-1 cursor-pointer hover:bg-orange-500 hover:text-white hover:transition ease-in-out duration-700 "></i>
       </div>
       
     
@@ -64,7 +72,7 @@ fetch("https://dummyjson.com/products")
       productSection.appendChild(card);
       const addToCartButton = card.querySelector(".add-to-cart");
       addToCartButton.addEventListener("click", () => {
-        // const productImg = document.getElementById("Product-img");
+        const productImg = product.thumbnail;
 
         const productName = product.title;
         const productPrice = product.price;
@@ -75,7 +83,7 @@ fetch("https://dummyjson.com/products")
           const product = {
             name: productName,
             price: productPrice,
-            // image: productImg,
+            image: productImg,
           };
           cart.push(product);
           updateCart();
@@ -86,7 +94,8 @@ fetch("https://dummyjson.com/products")
     });
   });
 
-  // ---------  -------//
+// ---------  -------//
 cartButton.addEventListener("click", () => {
   cartPrompt.classList.toggle("hidden");
+  // cartButton.classList.toggle("block");
 });
