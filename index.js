caf398a42ab8771ea0ed58fea681901ab5c15945
Fuad Khan <fuadkhanantor1@gmail.com>
2023-09-06 16:34:59 +0600

    Total price & delete functionality added

--- index.js
+++ index.js
@@ -2,15 +2,30 @@ const cart = [];
 let totalPrice = 0;
 
 const addToCart = document.querySelectorAll(".add-to-cart");
+const totalPriceElement = document.getElementById("total-price");
 const cartItemsList = document.getElementById("cart-items");
+const cartPrompt = document.querySelector(".cart-section");
+const cartButton = document.getElementById("cart-button");
 
 const updateCart = () => {
   cartItemsList.innerHTML = "";
+  totalPrice = 0;
 
-  cart.forEach((item) => {
+  cart.forEach((item, index) => {
     const listItem = document.createElement("li");
-    listItem.innerHTML = `${item.name}`;
+    listItem.innerHTML = `${item.name} => Price: ${item.price} <button class="rounded-lg p-2 hover:bg-red-500 bg-red-300 text-white pi-2 mb-2 ml-2 delete-item" data-index="${index}">Remove</button>`;
     cartItemsList.appendChild(listItem);
+    totalPrice += item.price;
+  });
+  totalPriceElement.innerHTML = `Total: $${totalPrice}`;
+
+  const deleteButtons = document.querySelectorAll(".delete-item");
+  deleteButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+      const index = button.getAttribute("data-index");
+      cart.splice(index, 1);
+      updateCart();
+    });
   });
 };
 
@@ -18,8 +33,15 @@ addToCart.forEach((button, index) => {
   button.addEventListener("click", () => {
     const productName =
       document.querySelectorAll(".card h2")[index].textContent;
-    const product = { name: productName };
+    const productPrice = parseFloat(
+      document.querySelectorAll(".card p")[index].textContent
+    );
+    const product = { name: productName, price: productPrice };
     cart.push(product);
     updateCart();
   });
+});
+
+cartButton.addEventListener("click", () => {
+  cartPrompt.classList.toggle("hidden");
 });
